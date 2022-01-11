d36b451b61e62e416369ea2ad99999d45c431a35
Agus Cuello <83506872+ChichiCuello@users.noreply.github.com>
2022-01-11 11:59:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,10 +37,10 @@ if (dateInput.value === ""){
     //calculate age
     if ( yearB < year && monthB<= month && dayB<= day ){
         var intAge = year - yearB 
-        var koreanAge = intAge + 2
+        var koreanAge = intAge + 1
     } else{
         var intAge = year - yearB -1 
-        var koreanAge = year - yearB +1
+        var koreanAge = year - yearB + 2
     };
 document.getElementById('age').innerHTML = 'Your international age is: ' + intAge +"<br/>"+ 'Your korean age is: ' + koreanAge;
 
